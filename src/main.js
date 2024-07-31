78970e2c1576380cf0cf2a91877bbe630cb2e16b
Kehinde <ajibadekehinde1@gmail.com>
2024-07-31 02:14:29 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import HighchartsVue from 'highcharts-vue';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBpkzk2q2sVRV6bR-coejiLSJEFuSfSZww',
+  authDomain: 'ehealth4everyone-8816a.firebaseapp.com',
+  projectId: 'ehealth4everyone-8816a',
+  storageBucket: 'ehealth4everyone-8816a.appspot.com',
+  messagingSenderId: '850242775580',
+  appId: '1:850242775580:web:ea7ce70cd5b48365790dd9',
+};
+
+const firebaseapp = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseapp);
+console.log(db);
+
+const app = createApp(App);
+
+console.log(firebaseapp);
+app.use(HighchartsVue);
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
---
+++ src/pages/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <div id="container" style="width: 80%; height: 400px"></div>
+  </div>
+</template>
+
+<script setup>
+import { db } from '@/main.js';
+import { collection, getDocs } from 'firebase/firestore';
+import { ref, onMounted } from 'vue';
+import { defineComponent } from 'vue';
+
+const persons = ref([]);
+
+const fetchPersons = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'ehealth4everyone'));
+    persons.value = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    const seriesData = processData(persons.value);
+    updateChart(seriesData);
+
+    // Store the processed data in localStorage
+    localStorage.setItem('chartData', JSON.stringify(seriesData));
+
+    console.log(persons.value);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const loadChartDataFromLocalStorage = () => {
+  const storedData = localStorage.getItem('chartData');
+  if (storedData) {
+    const seriesData = JSON.parse(storedData);
+    console.log(seriesData);
+    updateChart(seriesData);
+  } else {
+    // Fetch data from Firestore if not available in localStorage
+    fetchPersons();
+  }
+};
+
+onMounted(() => {
+  loadChartDataFromLocalStorage();
+});
+
+const processData = (persons) => {
+  const ageRanges = ['10-19', '20-29', '30-39'];
+  const bloodGroups = ['A', 'B', 'AB', 'O'];
+
+  const data = {
+    A: Array(ageRanges.length).fill(0),
+    B: Array(ageRanges.length).fill(0),
+    AB: Array(ageRanges.length).fill(0),
+    O: Array(ageRanges.length).fill(0),
+  };
+
+  persons.forEach((person) => {
+    const age = person.age;
+    const bloodGroup = person['blood-group'];
+    const rangeIndex = ageRanges.findIndex((range) => {
+      const [min, max] = range.split('-').map(Number);
+      return age >= min && age <= max;
+    });
+
+    if (rangeIndex !== -1) {
+      data[bloodGroup][rangeIndex]++;
+    }
+  });
+
+  return bloodGroups.map((group) => ({
+    name: group,
+    data: data[group],
+  }));
+};
+
+const updateChart = (seriesData) => {
+  Highcharts.chart('container', {
+    chart: {
+      type: 'column',
+    },
+    title: {
+      text: 'Blood Group Data of 20 Patients',
+      align: 'left',
+    },
+    xAxis: {
+      categories: ['10-19', '20-29', '30-39'],
+      crosshair: true,
+      accessibility: {
+        description: 'Ages',
+      },
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Number of Patients',
+      },
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0,
+      },
+    },
+    series: seriesData,
+  });
+};
+</script>
